6c4ff2379fe49770b545a516c738c45966fa2bfb
AleVinSan <alevineglas@uma.es>
2025-11-27 12:53:52 +0100

    Carga inicial del proyecto Spring Boot

---
+++ src/main/java/com/grupob/pastillero_backend/PastilleroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.grupob.pastillero_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastilleroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PastilleroBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupob/pastillero_backend/PastilleroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupob.pastillero_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastilleroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
